7140adb22cf3c8d9d23f23c461c846b8dd84ee9d
Dương Quốc Thái <quocthai261@gmail.com>
2023-08-18 03:20:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import About from "./routes/About.js";
 import Service from "./routes/Service.js";
 import Contact from "./routes/Contact.js";
 import Navbar from './components/Navbar';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdxO93s9F9eYNistfUPCjOHHyuzFJAV7o",
+  authDomain: "travel-website-c23b2.firebaseapp.com",
+  projectId: "travel-website-c23b2",
+  storageBucket: "travel-website-c23b2.appspot.com",
+  messagingSenderId: "193546402804",
+  appId: "1:193546402804:web:d2af19faf4e818961457a1",
+  measurementId: "G-QE8H2LW50G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
